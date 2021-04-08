57e46a245dc6f99edbd4cbef40a25439df071bf1
Jacob <jacobbuck@users.noreply.github.com>
2021-04-08 20:55:45 +1200

    Use Array.from to support any Iterator

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const defaultComparator = (a, b) =>
   ((a = `${a}`) < (b = `${b}`) ? -1 : a > b ? 1 : 0);
 
 const sortIndexes = (array, comparator = defaultComparator) =>
-  array
-    .map((value, index) => [index, value])
+  Array.from(array, (value, index) => [index, value])
     .sort(([,a], [,b]) => comparator(a, b))
     .map(([index]) => index);
 
