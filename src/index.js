6559c05121678949b5670330229f6ee920d11e5e
Jacob Buck <buck.jacob@gmail.com>
2021-04-10 08:10:13 +0000

    Prettier

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const defaultComparator = (a, b) => 
-  ((a = `${a}`) < (b = `${b}`) ? -1 : a > b ? 1 : 0);
+const defaultComparator = (a, b) =>
+  (a = `${a}`) < (b = `${b}`) ? -1 : a > b ? 1 : 0;
 
 const sortIndexes = (array, comparator = defaultComparator) =>
   Array.from(array, (value, index) => [index, value])
-    .sort(([,a], [,b]) => comparator(a, b))
+    .sort(([, a], [, b]) => comparator(a, b))
     .map(([index]) => index);
 
 export default sortIndexes;
