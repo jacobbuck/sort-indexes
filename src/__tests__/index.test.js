b84bf598cfec8ba2756e23692191e2accd5b6d4b
Jacob Buck <buck.jacob@gmail.com>
2021-04-11 05:31:57 +0000

    Update tests to handle equal entries

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,10 +1,10 @@
 import sortIndexes from '..';
 
 it('sorts an array', () => {
-  expect(sortIndexes(['foo', 'bar', 'oxo'])).toEqual([1, 0, 2]);
+  expect(sortIndexes(['foo', 'bar', 'oxo', 'bar'])).toEqual([1, 3, 0, 2]);
 });
 
 it('sorts an array with a comparator function', () => {
   const comparator = (a, b) => b - a;
-  expect(sortIndexes([5, 1, 3, 4, 2], comparator)).toEqual([0, 3, 2, 4, 1]);
+  expect(sortIndexes([5, 1, 3, 4, 1], comparator)).toEqual([0, 3, 2, 1, 4]);
 });
